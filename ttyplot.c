4c3c989d02ac1e0f177e0b09f35fb69c105a0c90
Antoni Sawicki <as@tenoware.com>
2018-10-23 23:50:52 -0700

    ver bump

--- ttyplot.c
+++ ttyplot.c
@@ -13,7 +13,7 @@
 #include <curses.h>
 #include <signal.h>
 
-#define verstring "github.com/tenox7/ttyplot 1.0"
+#define verstring "github.com/tenox7/ttyplot 1.1"
 
 void usage() {
     printf("Usage:\n ttyplot [-2] [-r] [-c plotchar] [-s softmax] [-m hardmax] [-t title] [-u unit]\n\n"
