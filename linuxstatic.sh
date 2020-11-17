49a18a00c8590cd3c5eda8f5ff0822fe488d824f
Antoni Sawicki <tenox@google.com>
2020-11-17 11:11:07 -0800

    Update linuxstatic.sh

--- linuxstatic.sh
+++ linuxstatic.sh
@@ -1,20 +1,17 @@
-#!/bin/bash -xe
 # overcomplicated method of building static version of ttyplot for linux
 docker rmi -f static
 cat <<EOF | docker build -t static  -f- .
 FROM ubuntu:xenial
 RUN apt-get update -qq
-RUN apt-get install -qq -o=Dpkg::Use-Pty=0 build-essential file git curl vim-tiny
+RUN apt-get install -qq -o=Dpkg::Use-Pty=0 build-essential file curl
 RUN echo "\n\
 rm -rf /ttyplot* /ncurses*\n\
-git clone https://github.com/tenox7/ttyplot.git\n\
 curl -Ls ftp://ftp.invisible-island.net/ncurses/ncurses.tar.gz | tar xzf -\n\
 cd ncurses*\n\
 ./configure -q --with-terminfo-dirs=/etc/terminfo:/lib/terminfo:/usr/lib/terminfo:/usr/share/terminfo\n\
-make -s\n\
-mv include/ lib/libncurses.a /ttyplot\n\
-cd /ttyplot\n\
-gcc -Iinclude ttyplot.c -static -o /out/ttyplot-amd64-linux libncurses.a\n\
+make -s -j$(nproc)\n\
+curl -LOs https://raw.githubusercontent.com/tenox7/ttyplot/master/ttyplot.c\n\
+gcc -Iinclude ttyplot.c -static -o /out/ttyplot-amd64-linux lib/libncurses.a\n\
 strip /out/ttyplot-amd64-linux\n\
 file /out/ttyplot-amd64-linux\n\
 " > /build.sh
