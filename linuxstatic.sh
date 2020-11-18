8e55fcd5643cf6a55d422ea749db754e7affab8a
Antoni Sawicki <tenox@google.com>
2020-11-17 20:12:50 -0800

    Update linuxstatic.sh

--- linuxstatic.sh
+++ linuxstatic.sh
@@ -6,7 +6,7 @@ FROM ubuntu:xenial
 RUN apt-get update -qq
 RUN apt-get install -qq -o=Dpkg::Use-Pty=0 build-essential file curl
 RUN echo "\n\
-rm -rf /ttyplot* /ncurses*\n\
+rm -rf /ncurses*\n\
 curl -Ls ftp://ftp.invisible-island.net/ncurses/ncurses.tar.gz | tar xzf -\n\
 cd ncurses*\n\
 ./configure -q --with-terminfo-dirs=/etc/terminfo:/lib/terminfo:/usr/lib/terminfo:/usr/share/terminfo\n\
