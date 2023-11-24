cd023692ef06a56d73e90e0b67cb99855dc2d697
Edgar Bonet <linux@edgar-bonet.org>
2023-11-24 18:47:03 +0100

    Let stresstest parse its command line
    The unnamed option used to enable two-wave output is now "-2". The new
    option "-h" prints an option summary.

--- stresstest.c
+++ stresstest.c
@@ -6,17 +6,44 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 #include <unistd.h>
 #include <math.h>
 
-int main(int argc, char *argv[]) {
-    (void) argv;
-    unsigned int n;
+const char help[] =
+    "Usage: %s [-h] [-2]\n"
+    "  -h       print this help message and exit\n"
+    "  -2       output two waves\n";
 
-    for(n=0;;n+=5) {
+const char optstring[] = "h2";
+
+int main(int argc, char *argv[]) {
+    int opt;
+    bool two_waves = false;
+
+    // Parse the command line.
+    while ((opt = getopt(argc, argv, optstring)) != -1) {
+        switch (opt) {
+            case 'h':
+                printf(help, argv[0]);
+                return EXIT_SUCCESS;
+            case '2':
+                two_waves = true;
+                break;
+            default:
+                fprintf(stderr, help, argv[0]);
+                return EXIT_FAILURE;
+        }
+    }
+    if (argc > optind) {
+        fprintf(stderr, help, argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    for (unsigned int n=0; ; n+=5) {
         printf("%.1f\n", (sin(n*M_PI/180)*5)+5);
-        if(argc==2)
+        if(two_waves)
             printf("%.1f\n", (cos(n*M_PI/180)*5)+5);
         fflush(stdout);
         usleep(10000);
