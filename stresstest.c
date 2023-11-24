d3393a016f15925bfc9d7cf3898f8ad1eae0a881
Edgar Bonet <linux@edgar-bonet.org>
2023-11-24 18:49:12 +0100

    stresstest.c: add option to set the sampling rate
    The option is "-r rate". Unit is samples per second.

--- stresstest.c
+++ stresstest.c
@@ -12,15 +12,17 @@
 #include <math.h>
 
 const char help[] =
-    "Usage: %s [-h] [-2]\n"
+    "Usage: %s [-h] [-2] [-r rate]\n"
     "  -h       print this help message and exit\n"
-    "  -2       output two waves\n";
+    "  -2       output two waves\n"
+    "  -r rate  sample rate in samples/s (default: 100)\n";
 
-const char optstring[] = "h2";
+const char optstring[] = "h2r:";
 
 int main(int argc, char *argv[]) {
     int opt;
     bool two_waves = false;
+    double rate = 100;
 
     // Parse the command line.
     while ((opt = getopt(argc, argv, optstring)) != -1) {
@@ -30,6 +32,9 @@ int main(int argc, char *argv[]) {
                 return EXIT_SUCCESS;
             case '2':
                 two_waves = true;
+                break;
+            case 'r':
+                rate = atof(optarg);
                 break;
             default:
                 fprintf(stderr, help, argv[0]);
@@ -41,12 +46,14 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
+    const useconds_t delay = 1e6 / rate;
+
     for (unsigned int n=0; ; n+=5) {
         printf("%.1f\n", (sin(n*M_PI/180)*5)+5);
         if(two_waves)
             printf("%.1f\n", (cos(n*M_PI/180)*5)+5);
         fflush(stdout);
-        usleep(10000);
+        usleep(delay);
     }
 
     return 0;
