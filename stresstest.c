994b48242b8d8e17b0fc36ec446b4acb81db57e8
Edgar Bonet <linux@edgar-bonet.org>
2023-11-24 19:53:41 +0100

    stresstest.c: add option to randomly chunk the output
    Option -c makes stresstest write its output in randomly-sized chunks,
    from 1 to 16 bytes.

--- stresstest.c
+++ stresstest.c
@@ -9,19 +9,24 @@
 #include <stdbool.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
 #include <math.h>
 
 const char help[] =
-    "Usage: %s [-h] [-2] [-r rate]\n"
+    "Usage: %s [-h] [-2] [-c] [-r rate]\n"
     "  -h       print this help message and exit\n"
     "  -2       output two waves\n"
+    "  -c       randomly chunk the output\n"
     "  -r rate  sample rate in samples/s (default: 100)\n";
 
-const char optstring[] = "h2r:";
+const char optstring[] = "h2cr:";
 
 int main(int argc, char *argv[]) {
+    char buffer[1024];
+    size_t buffer_pos = 0;
     int opt;
     bool two_waves = false;
+    bool chunked = false;
     double rate = 100;
 
     // Parse the command line.
@@ -32,6 +37,9 @@ int main(int argc, char *argv[]) {
                 return EXIT_SUCCESS;
             case '2':
                 two_waves = true;
+                break;
+            case 'c':
+                chunked = true;
                 break;
             case 'r':
                 rate = atof(optarg);
@@ -49,10 +57,24 @@ int main(int argc, char *argv[]) {
     const useconds_t delay = 1e6 / rate;
 
     for (unsigned int n=0; ; n+=5) {
-        printf("%.1f\n", (sin(n*M_PI/180)*5)+5);
+        buffer_pos += sprintf(buffer + buffer_pos, "%.1f\n", (sin(n*M_PI/180)*5)+5);
         if(two_waves)
-            printf("%.1f\n", (cos(n*M_PI/180)*5)+5);
-        fflush(stdout);
+            buffer_pos += sprintf(buffer + buffer_pos, "%.1f\n", (cos(n*M_PI/180)*5)+5);
+        if (chunked) {
+            size_t send_pos = 0;
+            while (buffer_pos - send_pos >= 16) {
+                const size_t bytes_to_send = 1 + rand() % 16;  // 1..16
+                write(STDOUT_FILENO, buffer + send_pos, bytes_to_send);
+                usleep(50);  // let ttyplot read this before proceeding
+                send_pos += bytes_to_send;
+            }
+            if (send_pos > 0 && send_pos < buffer_pos)
+                memmove(buffer, buffer + send_pos, buffer_pos - send_pos);
+            buffer_pos -= send_pos;
+        } else {
+            write(STDOUT_FILENO, buffer, buffer_pos);
+            buffer_pos = 0;
+        }
         usleep(delay);
     }
 
