a47053fe1e8b535f662e51ff248ff1a98da6d540
Sebastian Pipping <sebastian@pipping.org>
2023-12-25 16:31:08 +0100

    Bump version to 1.6.1

--- ttyplot.c
+++ ttyplot.c
@@ -39,7 +39,7 @@
 #define STR(x) STR_(x)
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 6
-#define VERSION_PATCH 0
+#define VERSION_PATCH 1
 #define VERSION_STR STR(VERSION_MAJOR) "." STR(VERSION_MINOR) "." STR(VERSION_PATCH)
 
 #define T_RARR '>'
